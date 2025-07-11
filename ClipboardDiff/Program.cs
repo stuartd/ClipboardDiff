0fcb93a3879c5ad7dffdc61b2ac44a110bcf41e7
stuartd <stuartd@gmail.com>
2025-07-11 12:55:44 +0100

    Updated to allow for temp files with spaces in
    Also -> .net 9

--- ClipboardDiff/Program.cs
+++ ClipboardDiff/Program.cs
@@ -11,9 +11,10 @@ namespace ClipboardDiff {
 			string text1 = null;
 			string text2 = null;
 
-			var sql = "select ooData from data where strClipBoardFormat = 'CF_TEXT' and LParentId in (select lId from main order by lID desc limit 2)";
+			const string sql = "select ooData from data where strClipBoardFormat = 'CF_TEXT' and LParentId in (select lId from main order by lID desc limit 2)";
 			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ditto", "Ditto.db;Version=3;");
-			using (var connection = new SQLiteConnection($"Data Source={path}")) {
+
+            using (var connection = new SQLiteConnection($"Data Source={path}")) {
 				using (var command = new SQLiteCommand(sql, connection)) {
 					command.Prepare();
 					connection.Open();
@@ -42,13 +43,17 @@ namespace ClipboardDiff {
 				var tempFile2 = Path.GetTempFileName();
 				File.WriteAllText(tempFile2, text2);
 
-				var processData = new ProcessStartInfo {
-					FileName = @"C:\Program Files\SourceGear\Common\DiffMerge\sgdm.exe",
-					Arguments = $"/title1=1 /title2=2 {tempFile1} {tempFile2}",
-					UseShellExecute = true
-				};
+                var psi = new ProcessStartInfo
+                {
+                    FileName = @"C:\Program Files\SourceGear\Common\DiffMerge\sgdm.exe"
+                };
+                psi.ArgumentList.Add("/title1=Older");
+                psi.ArgumentList.Add("/title2=Newer");
+                psi.ArgumentList.Add(tempFile1);
+                psi.ArgumentList.Add(tempFile2);
+                psi.UseShellExecute = true;
 
-				Process.Start(processData);
+                Process.Start(psi);
 			}
 		}
 	}
