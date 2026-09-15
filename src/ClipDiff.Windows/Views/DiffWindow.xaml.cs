1605d0c363c45d447fa39efec42a9e5e1c268760
stuartd <stuartd@gmail.com>
2026-09-15 21:23:14 +0100

    Use dark mode for built-in diff viewer

--- src/ClipDiff.Windows/Views/DiffWindow.xaml.cs
+++ src/ClipDiff.Windows/Views/DiffWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Interop;
+using ClipDiff.Windows.Native;
 
 namespace ClipDiff.Windows.Views;
 
@@ -11,6 +13,26 @@ public partial class DiffWindow : Window
     }
 
     public bool AllowClose { get; set; }
+
+    private void OnSourceInitialized(object? sender, EventArgs args)
+    {
+        var windowHandle = new WindowInteropHelper(this).Handle;
+        var enabled = 1;
+        var result = NativeMethods.DwmSetWindowAttribute(
+            windowHandle,
+            NativeMethods.DwmwaUseImmersiveDarkMode,
+            ref enabled,
+            sizeof(int));
+
+        if (result != 0)
+        {
+            NativeMethods.DwmSetWindowAttribute(
+                windowHandle,
+                NativeMethods.DwmwaUseImmersiveDarkModeBefore20H1,
+                ref enabled,
+                sizeof(int));
+        }
+    }
 
     private void OnClosing(object? sender, CancelEventArgs args)
     {
